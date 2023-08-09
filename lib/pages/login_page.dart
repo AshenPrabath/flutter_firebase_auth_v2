715c0fb3c1d5a191ade82e6cf20f6540c66b1c1e
Ashen Prabath <ashenprabath31@gmail.com>
2023-08-10 02:44:34 +0530

    firebase setup,login validation done

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,112 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  bool isLoading = false;
+
+  signInWithEmailAndPassword() async {
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _email.text,
+        password: _password.text,
+      );
+      setState(() {
+        isLoading = false;
+      });
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      if (e.code == 'user-not-found') {
+        return ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Wrong Email"),
+          ),
+        );
+      } else if (e.code == 'wrong-password') {
+        return ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Wrong password"),
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: OverflowBar(
+              overflowSpacing: 15,
+              children: [
+                TextFormField(
+                  controller: _email,
+                  validator: (text) {
+                    if (text == null || text.isEmpty) {
+                      return 'Email is empty';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(labelText: "Email"),
+                ),
+                TextFormField(
+                  controller: _password,
+                  validator: (text) {
+                    if (text == null || text.isEmpty) {
+                      return 'password is empty';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(labelText: "password"),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        signInWithEmailAndPassword();
+                      }
+                    },
+                    child: isLoading
+                        ? const Center(
+                            child: CircularProgressIndicator(
+                            color: Colors.white,
+                          ))
+                        : const Text("Login"),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("Register"),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
